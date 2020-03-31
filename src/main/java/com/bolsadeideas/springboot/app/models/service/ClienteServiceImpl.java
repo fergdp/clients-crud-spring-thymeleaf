7d0d98304908f9b2c276a01b8948357b3b351c9e
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2020-03-31 16:15:04 -0300

    join fetch cliente

--- src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImpl.java
@@ -47,7 +47,13 @@ public class ClienteServiceImpl implements IClienteService {
 		// TODO Auto-generated method stub
 		return clienteDao.findById(id).orElse(null);
 	}
-
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente fetchByIdWithFacturas(Long id) {
+		return clienteDao.fetchByIdWithFacturas(id);
+	}
+	
 	@Override
 	@Transactional
 	public void delete(Long id) {
